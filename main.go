aae305e88eed0695d78c966f34632d094b82d1d1
Todd Hainsworth <hainsworth.todd@gmail.com>
2018-07-29 21:59:02 +0930

    initial short-report

--- main.go
+++ main.go
@@ -1,21 +1,76 @@
 package main
 
 import (
-  "fmt"
-  "github.com/jason0x43/go-toggl"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jason0x43/go-toggl"
 )
 
-const APIKEY = "<goes-here>"
-
 func main() {
-  toggl.DisableLog()
-  session := toggl.OpenSession(APIKEY)
-  account, err := session.GetAccount()
+	toggl.DisableLog()
+	apiKey, err := getAPIKey()
 
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  fmt.Println(account.Data.BeginningOfWeek)
+	session := toggl.OpenSession(apiKey)
+	account, err := session.GetAccount()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	start, end := getDates()
+	for _, workspace := range account.Data.Workspaces {
+		// Only get the first page, any more data and we'd likely fill the terminal
+		report, err := session.GetDetailedReport(workspace.ID, start, end, 1)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, reportItem := range report.Data {
+			fmt.Printf("%s - %d\n", reportItem.Description, reportItem.Duration)
+		}
+	}
+}
+
+func getAPIKey() (string, error) {
+	data, err := ioutil.ReadFile(os.ExpandEnv("$HOME/.gottl"))
+
+	if err != nil {
+		return "", err
+	}
+
+	str := string(data)
+	return strings.TrimSpace(str), nil
+}
+
+func getDates() (start, end string) {
+	endDate := time.Now()
+	y, m, d := endDate.Date()
+	end = fmt.Sprintf("%d-%d-%d", y, m, d)
+
+	// Subtract 5 days
+	startDate := endDate.AddDate(0, 0, -5)
+	y, m, d = startDate.Date()
+	start = fmt.Sprintf("%d-%d-%d", y, m, d)
+	return
+
+}
+
+func msToS(ms int64) int64 {
+	return ms / 1000
+}
+
+func ppStruct(data interface{}) {
+	fmt.Printf("%+v\n", data)
 }
