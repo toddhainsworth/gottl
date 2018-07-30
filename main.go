6979eb63603cb6ff4a356f22f04087106be9a59c
Todd Hainsworth <hainsworth.todd@gmail.com>
2018-07-30 22:07:28 +0930

    time formats

--- main.go
+++ main.go
@@ -37,19 +37,49 @@ func main() {
 			return
 		}
 
+		fmt.Printf("Report for \"%s\"\n", workspace.Name)
+
+		itemsByTime := make(map[string][]toggl.DetailedTimeEntry, len(report.Data))
 		for _, reportItem := range report.Data {
-            time, err := getTime(reportItem.Duration)
+			startString := getStartString(*reportItem.Start)
+			// If the map has the time key already then we can append, otherwise insert
+			if _, ok := itemsByTime[startString]; ok {
+				itemsByTime[startString] = append(itemsByTime[startString], reportItem)
+			} else {
+				itemsByTime[startString] = []toggl.DetailedTimeEntry{reportItem}
+			}
+			// printItem(reportItem)
+		}
 
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-
-			fmt.Printf("%s - %v\n", reportItem.Description, time)
+		for time, items := range itemsByTime {
+			fmt.Println(time)
+			for _, item := range items {
+				printItem(item)
+			}
 		}
 	}
 }
 
+func getStartString(time time.Time) string {
+	return fmt.Sprintf("-- %s %d/%d/%d --",
+		time.Weekday(), time.Day(), time.Month(), time.Year())
+}
+
+// Print the given item in the regular format
+func printItem(item toggl.DetailedTimeEntry) {
+	dur, err := getDuration(item.Duration)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	startTime, endTime := item.Start.Format("01:02"), item.End.Format("01:02")
+	fmt.Printf("(%s - %s) %s - %s\n",
+		startTime, endTime, item.Description, dur.String())
+}
+
+// Get the API key from the .gottl file
 func getAPIKey() (string, error) {
 	data, err := ioutil.ReadFile(os.ExpandEnv("$HOME/.gottl"))
 
@@ -61,6 +91,7 @@ func getAPIKey() (string, error) {
 	return strings.TrimSpace(str), nil
 }
 
+// Get the start and end dates to send to the Toggl API
 func getDates() (start, end string) {
 	endDate := time.Now()
 	y, m, d := endDate.Date()
@@ -74,14 +105,12 @@ func getDates() (start, end string) {
 
 }
 
-func getTime(ms int64) (time.Duration, error) {
-    return time.ParseDuration(fmt.Sprintf("%dms", ms))
+// Converts the duration in 'ms' to long-form
+func getDuration(ms int64) (time.Duration, error) {
+	return time.ParseDuration(fmt.Sprintf("%dms", ms))
 }
 
-func msToS(ms int64) int64 {
-	return ms / 1000
-}
-
+// Helper function to pretty-print the struct
 func ppStruct(data interface{}) {
 	fmt.Printf("%+v\n", data)
 }
